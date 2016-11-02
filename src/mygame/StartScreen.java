a04d641c3c1bed19869c05aa6a6cf86eb55edf77
ekaterinaMaljutina <e.maljutina@gmail.com>
2016-11-02 11:16:36 +0300

    delete coments

--- src/mygame/StartScreen.java
+++ src/mygame/StartScreen.java
@@ -9,9 +9,6 @@ import de.lessvoid.nifty.elements.render.TextRenderer;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 
-/**
- *
- */
 public class StartScreen extends AbstractAppState implements ScreenController {
 
   private Nifty nifty;
@@ -20,10 +17,10 @@ public class StartScreen extends AbstractAppState implements ScreenController {
   
   private MainMenu owner;
   
-  /** custom methods */
+ 
   public StartScreen(MainMenu owner) {
       this.owner = owner;
-    /** You custom constructor, can accept arguments */
+  
   }
   public void startServer()  {
     System.out.println("server");
@@ -34,7 +31,7 @@ public class StartScreen extends AbstractAppState implements ScreenController {
   }
 
   public void startGame(String nextScreen) {
-    nifty.gotoScreen(nextScreen);  // switch to another screen
+    nifty.gotoScreen(nextScreen);  
     owner.init_player();
   }
 
@@ -46,7 +43,6 @@ public class StartScreen extends AbstractAppState implements ScreenController {
     return System.getProperty("user.name");
   }
 
-  /** Nifty GUI ScreenControl methods */
   public void bind(Nifty nifty, Screen screen) {
     this.nifty = nifty;
     this.screen = screen;
@@ -58,7 +54,6 @@ public class StartScreen extends AbstractAppState implements ScreenController {
   public void onEndScreen() {
   }
 
-  /** jME3 AppState methods */
   @Override
   public void initialize(AppStateManager stateManager, Application app) {
     this.app = app;
@@ -69,7 +64,6 @@ public class StartScreen extends AbstractAppState implements ScreenController {
       
     if (screen.getScreenId().equals("hud")) {
       Element niftyElement = nifty.getCurrentScreen().findElementByName("score");
-      // Display the time-per-frame -- this field could also display the score etc...
       niftyElement.getRenderer(TextRenderer.class).setText((int)(tpf*100000) + ""); 
     }
     
