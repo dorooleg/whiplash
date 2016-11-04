e09de88264f942db4fb8d9cea7465a641263d2da
ekaterinaMaljutina <e.maljutina@gmail.com>
2016-11-04 10:52:29 +0300

    work version

--- src/mygame/MainMenu.java
+++ src/mygame/MainMenu.java
@@ -44,16 +44,15 @@ public class MainMenu extends SimpleApplication implements ActionListener,
     private Node[] node_whip1;
     private Node[] node_whip2;
     private float time_simple_update = 0f;
-    private static final float WHIP_WIDTH  = 30f;
+    private static final float WHIP_WIDTH = 30f;
     private static final float WHIP_HEIGHT = 30f;
     private static final float PLAYER_BODY_WIDTH = 30f;
     private static final float PLAYER_BODY_HEIGHT = 30f;
     private Sound sound;
     private ColorRGBA colorPlayer;
     private ColorRGBA colorPlayer2;
-
     private float health_status = 1f;
-    
+
     public MainMenu() {
         listEvents = new ArrayList<Callable>();
         shortListEvents = new ArrayList<Callable>();
@@ -91,7 +90,7 @@ public class MainMenu extends SimpleApplication implements ActionListener,
         Serializer.registerClass(ProtocolMessage.class);
         Serializer.registerClass(ProtocolMessage.Entry.class);
         sound = new Sound(assetManager);
-        
+
         sound.startMusic();
     }
 
@@ -131,13 +130,14 @@ public class MainMenu extends SimpleApplication implements ActionListener,
 
         if (name.equals("Mouse_Click_Left")) {
             player.getControl(PlayerControl.class).mouse_pressed = isPressed;
-//            
-            
+
             if (startScreen.client != null) {
+                System.out.println(player.getControl(PlayerControl.class).getHealth());
                 startScreen.client.send(getProtocolMessage());
             }
 
             if (startScreen.server != null) {
+                System.out.println(player.getControl(PlayerControl.class).getHealth());
                 startScreen.server.broadcast(getProtocolMessage());
             }
 
@@ -148,10 +148,10 @@ public class MainMenu extends SimpleApplication implements ActionListener,
 
         if (startScreen.server != null) {
             colorPlayer = ColorRGBA.Red;
-            colorPlayer2 = ColorRGBA.Green;
+            colorPlayer2 = ColorRGBA.Blue;
         } else {
+            colorPlayer = ColorRGBA.Blue;
             colorPlayer2 = ColorRGBA.Red;
-            colorPlayer = ColorRGBA.Green;
         }
 
         initCamera();
@@ -198,9 +198,6 @@ public class MainMenu extends SimpleApplication implements ActionListener,
 
         startScreen.setProgress(player.getControl(PlayerControl.class).getHealth());
         startScreen.setProgressWhip(player.getControl(PlayerControl.class).getWhipStatus());
-        
-
-
 
         player2 = getSpatial("player2");
         ((Node) player2).attachChild(node_whip2[0]);
@@ -224,11 +221,9 @@ public class MainMenu extends SimpleApplication implements ActionListener,
         guiNode.attachChild(player2);
     }
 
-
     void clearPlayers() {
         guiNode.detachAllChildren();
     }
-
 
     @Override
     public void onAnalog(String name, float value, float tpf) {
@@ -237,11 +232,12 @@ public class MainMenu extends SimpleApplication implements ActionListener,
                     || name.equals("Left")
                     || name.equals("Down")
                     || name.equals("Up")
-                    || name.equals("MouseMove")) {
+                    || name.equals("MouseMoved")) {
                 player.getControl(PlayerControl.class).mouse_position =
                         inputManager.getCursorPosition();
                 player.getControl(PlayerControl.class).move_mouse = true;
 
+//                sound.shoot();
                 if (startScreen.client != null) {
                     startScreen.client.send(getProtocolMessage());
                 }
@@ -249,7 +245,6 @@ public class MainMenu extends SimpleApplication implements ActionListener,
                 if (startScreen.server != null) {
                     startScreen.server.broadcast(getProtocolMessage());
                 }
-//                sound.shoot();
             }
         }
 
@@ -262,18 +257,18 @@ public class MainMenu extends SimpleApplication implements ActionListener,
                     if (message.entries.isEmpty()) {
                         throw new Exception("Invalid protocol message size");
                     }
-//                    System.out.println("Callable = " + message);
                     player2.setLocalTranslation(message.entries.get(0).x,
                             message.entries.get(0).y, 0);
                     player2.setLocalRotation(message.entries.get(0).rotation);
 
                     player2.getControl(PlayerControl.class).mouse_pressed =
                             message.entries.get(0).whip_heat;
+
                     colorPlayer2 = message.entries.get(0).color;
 
-                    player2.getControl(PlayerControl.class).setHealth(
+                    player.getControl(PlayerControl.class).setHealth(
                             message.entries.get(0).health_status);
-                    
+
                     player2.getControl(PlayerControl.class).setWhipStatus(
                             message.entries.get(0).whip_status);
                     return null;
@@ -295,7 +290,7 @@ public class MainMenu extends SimpleApplication implements ActionListener,
                 }
                 listEvents.clear();
             }
-            
+
             if (!shortListEvents.isEmpty()) {
                 Iterator<Callable> iterator = shortListEvents.iterator();
                 while (iterator.hasNext()) {
@@ -314,14 +309,22 @@ public class MainMenu extends SimpleApplication implements ActionListener,
         if (player == null || player2 == null) {
             return;
         }
-        
-        handlCollision();
-        if (startScreen.server==null)
+
+        if (startScreen.server == null) {
+            startScreen.setProgress(player2.getControl(PlayerControl.class).getHealth());
+            startScreen.setProgress_Blue(player.getControl(PlayerControl.class).getHealth());
+
+            startScreen.setProgressWhip(player2.getControl(PlayerControl.class).getWhipStatus());
+            startScreen.setProgressWhip_blue(player.getControl(PlayerControl.class).getWhipStatus());
+
+        } else {
             startScreen.setProgressWhip(player.getControl(PlayerControl.class).getWhipStatus());
-        else
             startScreen.setProgressWhip_blue(player2.getControl(PlayerControl.class).getWhipStatus());
-//        startScreen.setProgressWhip_blue(player2.getControl(PlayerControl.class).getWhipStatus());
-        
+
+            startScreen.setProgress(player.getControl(PlayerControl.class).getHealth());
+            startScreen.setProgress_Blue(player2.getControl(PlayerControl.class).getHealth());
+        }
+
         if (player.getControl(PlayerControl.class).draw_flag == 1 && time_simple_update > 0.15f) {
 
             draw_Node("player1", 1, 2, false);
@@ -368,14 +371,33 @@ public class MainMenu extends SimpleApplication implements ActionListener,
             player2.getControl(PlayerControl.class).draw_flag = 0;
             time_simple_update = 0;
         }
+
+//        if (startScreen.client != null) {
+//            startScreen.client.send(getProtocolMessage());
+//        }
+//
+//        if (startScreen.server != null) {
+//            startScreen.server.broadcast(getProtocolMessage());
+//        }
     }
 
+    private void handleCollision() {
+        if (player.getControl(PlayerControl.class).inRadiusWhipHit(player2.getLocalTranslation())) {;
+            player2.getControl(PlayerControl.class).decreaseHealth();
 
-    private boolean handlCollision() {
-        return  player.getControl(PlayerControl.class).inRadiusWhipHit(player2.getLocalTranslation()) &&
-        player2.getControl(PlayerControl.class).inRadiusWhipHit(player.getLocalTranslation());
+//            if (startScreen.server == null) {
+//                startScreen.setProgress(player.getControl(PlayerControl.class).getHealth());
+//                startScreen.setProgress_Blue(player2.getControl(PlayerControl.class).getHealth());
+//            } else {
+//                startScreen.setProgress(player.getControl(PlayerControl.class).getHealth());
+//                startScreen.setProgress_Blue(player2.getControl(PlayerControl.class).getHealth());
+//            }
+
+        }
+//        if (player2.getControl(PlayerControl.class).inRadiusWhipHit(player.getLocalTranslation())){
+//            player.getControl(PlayerControl.class).decreaseHealth();
+//        }
     }
-
 
     private void initCamera() {
         cam.setLocation(new Vector3f(0.0f, 0.0f, 0.5f));
@@ -456,7 +478,7 @@ public class MainMenu extends SimpleApplication implements ActionListener,
         entry.y = player.getLocalTranslation().y;
         entry.whip_heat = player.getControl(PlayerControl.class).mouse_pressed;
         entry.color = colorPlayer;
-        entry.health_status = player.getControl(PlayerControl.class).getHealth();
+        entry.health_status = player2.getControl(PlayerControl.class).getHealth();
         entry.whip_status = player.getControl(PlayerControl.class).getWhipStatus();
         protocolMessage.addEntry(entry);
         return protocolMessage;
@@ -509,22 +531,18 @@ public class MainMenu extends SimpleApplication implements ActionListener,
     }
 
     public void drawWhip(String name_player) {
-//        sound.stopMusic();
         sound.whip();
-        sound.moan();
-        if  (handlCollision()){  
-            health_status = Math.max(health_status - 0.1f,0);
-            
-//            if (startScreen.server == null)
-//                startScreen.setProgress(player2.getControl(PlayerControl.class).getHealth());
-//            else
-//                startScreen.setProgress(player.getControl(PlayerControl.class).getHealth());
-            startScreen.setProgress_Blue(health_status);
-        }
+        handleCollision();
         draw_Node(name_player, 0, 1, false);
-        
-//        sound.shootStop();
-//        sound.startMusic();
+
+//        if (startScreen.server == null) {
+//            startScreen.setProgress(player.getControl(PlayerControl.class).getHealth());
+//            startScreen.setProgress_Blue(player2.getControl(PlayerControl.class).getHealth());
+//        } else {
+//            startScreen.setProgress(player.getControl(PlayerControl.class).getHealth());
+//            startScreen.setProgress_Blue(player2.getControl(PlayerControl.class).getHealth());
+//        }
+
     }
 
     private void draw_Node(String name_player, int node_num_detach,
@@ -534,7 +552,6 @@ public class MainMenu extends SimpleApplication implements ActionListener,
         if (name_player.equals("player1")) {
             playerNode = (Node) player;
             node = node_whip1;
-            
         }
         if (name_player.equals("player2")) {
             playerNode = (Node) player2;
@@ -543,10 +560,10 @@ public class MainMenu extends SimpleApplication implements ActionListener,
         if (playerNode == null || node == null) {
             return;
         }
-        if (startScreen.server != null)
-            startScreen.setProgress(playerNode.getControl(PlayerControl.class).getHealth());
-        else
-            startScreen.setProgress_Blue(playerNode.getControl(PlayerControl.class).getHealth());
+//        if (startScreen.server != null)
+//        startScreen.setProgress(playerNode.getControl(PlayerControl.class).getHealth());
+////          else
+//        startScreen.setProgress_Blue(playerNode.getControl(PlayerControl.class).getHealth());
 
         playerNode.detachChildNamed(node[node_num_detach].getName());
         playerNode.attachChild(node[node_num_attac]);
--- src/mygame/PlayerControl.java
+++ src/mygame/PlayerControl.java
@@ -25,31 +25,23 @@ public class PlayerControl extends AbstractControl {
     public Float speed = 100f,
             speed_mouse = 10f;
     public Vector2f mouse_position;
-    
-    
     private int width;
     private int height;
     private int width_body;
     private int height_body;
-    
-    
     private float health_status;
     private float whip_status;
     private final float speed_whip = 0.03f;
-    
-    private static final float WHIP_WIDTH  = 30f;
+    private static final float WHIP_WIDTH = 30f;
     private static final float WHIP_HEIGHT = 30f;
     private static final float PLAYER_BODY_WIDTH = 30f;
     private static final float PLAYER_BODY_HEIGHT = 30f;
     private static final float RADIUS = 2f * FastMath.sqr(WHIP_HEIGHT);
     private static final float WHIP_RADIUS = 10f * RADIUS;
-    private static  final  float WHIP_ANGLE = FastMath.PI / 12f;
- 
-    
+    private static final float WHIP_ANGLE = FastMath.PI / 12f;
     public int draw_flag;
-    
     private MainMenu owner;
-    
+
     public float getHealth() {
         return health_status;
     }
@@ -57,11 +49,15 @@ public class PlayerControl extends AbstractControl {
     public void setHealth(float health) {
         health_status = health;
     }
+    public void decreaseHealth() {
+        health_status = Math.max(health_status - 0.1f, 0f);
+    }
 
     public float getWhipStatus() {
         return whip_status;
     }
-     public void setWhipStatus(float value) {
+
+    public void setWhipStatus(float value) {
         whip_status = value;
     }
 
@@ -84,7 +80,7 @@ public class PlayerControl extends AbstractControl {
 
     @Override
     protected void controlUpdate(float tpf) {
-        
+
         if (rotate) {
             spatial.rotate(0, 0, FastMath.PI);
             rotate = false;
@@ -146,13 +142,11 @@ public class PlayerControl extends AbstractControl {
         if (mouse_was_pressed && !mouse_pressed) {
             if (whip_status == 1.f) {
                 draw_flag = 1;
-                health_status = Math.max(health_status - 0.1f, 0f);
                 owner.drawWhip(spatial.getName());
             }
-            Node cur = (Node)spatial;
-            Spatial whip =  cur.getChild("whip_spin_state");
-            System.err.println(whip);
-            if (whip != null){
+            Node cur = (Node) spatial;
+            Spatial whip = cur.getChild("whip_spin_state");
+            if (whip != null) {
                 whip.setLocalRotation(Matrix3f.IDENTITY);
             }
             mouse_was_pressed = false;
@@ -160,51 +154,45 @@ public class PlayerControl extends AbstractControl {
         }
     }
 
-    public boolean inRadiusWhipHit(Vector3f enemyPos){
-        Vector3f position_player  = spatial.getLocalTranslation();
-        
-        float p = FastMath.sqr(enemyPos.x - position_player.x) + 
-                  FastMath.sqr(enemyPos.y - position_player.y);
-        
+    public boolean inRadiusWhipHit(Vector3f enemyPos) {
+        Vector3f position_player = spatial.getLocalTranslation();
+
+        float p = FastMath.sqr(enemyPos.x - position_player.x)
+                + FastMath.sqr(enemyPos.y - position_player.y);
+
         Vector2f v = new Vector2f(enemyPos.x - position_player.x,
-                                  enemyPos.y - position_player.y);
+                enemyPos.y - position_player.y);
         float len = v.length();
-        
-        if (p  < WHIP_RADIUS) {
-            
+
+        if (p < WHIP_RADIUS) {
+
             float[] angle = {0, 0, 0};
             spatial.getWorldRotation().toAngles(angle);
-            
+
             float playerAngle = angle[2];
-            
-            if (angle[0] != 0)            
+
+            if (angle[0] != 0) {
                 playerAngle = angle[0] - angle[2];
-            else 
+            } else {
                 playerAngle = angle[2];
-            
-            
+            }
+
+
             Vector2f v1 = new Vector2f(FastMath.cos(playerAngle - WHIP_ANGLE),
-                                       FastMath.sin(playerAngle - WHIP_ANGLE));
+                    FastMath.sin(playerAngle - WHIP_ANGLE));
             Vector2f v2 = new Vector2f(FastMath.cos(playerAngle + WHIP_ANGLE),
-                                       FastMath.sin(playerAngle + WHIP_ANGLE));
-            
-            if (v1.x * v.y - v.x * v1.y > 0 &&
-                v2.x * v.y - v.x * v2.y < 0) {
-                System.out.println("collision");
+                    FastMath.sin(playerAngle + WHIP_ANGLE));
+
+            if (v1.x * v.y - v.x * v1.y > 0
+                    && v2.x * v.y - v.x * v2.y < 0) {
                 return true;
             }
-            
-            
-            
         }
-        
-        
-        
-        
         return false;
-        
+
     }
-    
+
     @Override
-    protected void controlRender(RenderManager rm, ViewPort vp) {}
+    protected void controlRender(RenderManager rm, ViewPort vp) {
+    }
 }
